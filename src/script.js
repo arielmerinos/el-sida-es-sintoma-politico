ce164f0d9741a174e8fb6a0c9061004359bafaa3
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-07 15:12:46 -0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,210 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+/**
+ * Debug
+ */
+// const gui = new GUI()
+
+const parameters = {
+    materialColor: '#d71414'
+}
+
+// gui
+//     .addColor(parameters, 'materialColor').onFinishChange(() => {
+//         material.color.set(parameters.materialColor)
+//     })
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * load texture
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('/textures/gradients/3.jpg')
+// Might need to change the filter to NearestFilter or not
+texture.magFilter = THREE.NearestFilter
+
+const objLoader = new GLTFLoader()
+objLoader.load(
+    '/models/ribbon.glb',
+    (gltf) => {
+        gltf.scene.scale.set(1.2, 1.2, 1.2)
+        gltf.scene.position.set(0, -1.5, -1)
+        gltf.scene.children[0].material = material
+        scene.add(gltf.scene)
+
+    }
+)
+
+/**
+ * Objects
+ */
+
+const material = new THREE.MeshToonMaterial({ color: parameters.materialColor, gradientMap: texture })
+
+
+const objectsDistance = 4
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60), 
+    material    
+)
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32), 
+    material  
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8,0.35, 100, 16), 
+    material
+)
+
+/**
+ * Positon
+ */
+
+mesh1.position.y = - objectsDistance * 0
+mesh1.visible = false
+mesh2.position.y = - objectsDistance * 1
+mesh2.position.x = 1.5
+
+mesh3.position.y = - objectsDistance * 2
+mesh3.position.x = - 1.5
+ 
+
+/**
+ * Light
+ */
+
+const directionalLight = new THREE.DirectionalLight("#fffff", 5)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock( )
+
+console.log(scene)
+
+
+/**
+ * Scroll view
+ */
+
+let scrollY = window.scrollY
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+    console.log('scroll', scrollY)
+})
+
+/**
+ * Cursor
+ * 
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    console.log('the mouse just moved')
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+    console.log(event.clientX / sizes.width, event.clientY / sizes.height)
+})
+
+/**
+ * Cursor section
+ */
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    
+    // Animate camera
+
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+    const parallaxX = cursor.x * 0.2
+    const parallaxY = cursor.y * 0.2
+    camera.position.x = parallaxX
+    cameraGroup.position.y = - parallaxY
+    camera.position.z = 6 + parallaxY
+    camera.rotation.y = - parallaxX * 0.1
+    camera.rotation.x = - parallaxY * 0.1
+
+
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.y = elapsedTime * 0.1
+        mesh.rotation.x = elapsedTime * 0.12
+    }
+
+    // Rotation of the ribbon mesh but applied to the whole group/camera
+    // scene.children[4].rotation.z =  -  elapsedTime * 0.04
+    // scene.children[4].rotation.x =  - Math.sin(elapsedTime * 0.01 * Math.PI) * 0.4
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
